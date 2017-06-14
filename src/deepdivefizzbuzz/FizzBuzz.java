ce0a1bc492b4170f0c51b5e856ecf4c310f85178
kpescobar <kellyp.721@live.com>
2017-06-14 06:44:20 -0700

    Testing Exception Messages

--- src/deepdivefizzbuzz/FizzBuzz.java
+++ src/deepdivefizzbuzz/FizzBuzz.java
@@ -1,12 +1,32 @@
 package deepdivefizzbuzz;
 
 public class FizzBuzz {
-
+    
+    /** Default upper limit for counting */
   private static final int DEFAULT_UPPER_LIMIT = 100;
+  
+  public static final String USAGE = 
+      "Usage:%n"
+      + "\tjava [options] edu.cnm.deepdive.fizzbuzz.FizzBuzz [limit]%n"
+      + "%n"
+      + "where%n"
+      + "\toptions = VM options%n"
+      + "\tlimit = positive upper counting limit (default = 100)";
+  
   public static void main(String[] args) {
+    try {
     int upperLimit = (args.length > 0) ? Integer.parseInt(args[0]): DEFAULT_UPPER_LIMIT; 
+    if (upperLimit <= 0) {
+      throw new IllegalArgumentException("Upper limit must be positive");
+    }
     emitFizzBuzz(upperLimit);
+    }
+    catch (IllegalArgumentException ex) {
+      ex.printStackTrace();
+      emitUsage();
+    }
   }
+  
   private static void emitFizzBuzz(int limit) {
    for (int i= 1; i<= limit; i++) {
      String response = "";
@@ -21,6 +41,10 @@ public class FizzBuzz {
      }
      System.out.println(response);
      }
+  }
+  
+  private static void emitUsage() {
+    System.out.printf(USAGE);
   }
 
 }
